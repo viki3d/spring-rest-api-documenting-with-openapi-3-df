cc205df8eb35a569c455cecbe40e058d3e255f9f
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-02 13:45:28 +0800

    Delete generate-with-jar-openapi-generator directory

--- generate-with-jar-openapi-generator/server/generate.sh
+++
@@ -1,3 +0,0 @@
-wget https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/5.2.1/openapi-generator-cli-5.2.1.jar
-java -jar openapi-generator-cli-5.2.1.jar generate -i ../../viki3d-cars-api-v1.yaml -o generated-sources -c config.json
-
