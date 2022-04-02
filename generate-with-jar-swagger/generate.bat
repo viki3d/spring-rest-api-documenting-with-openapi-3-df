17319b33b4b9d3c2a6a0e28582414f0a502f2307
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-02 13:46:43 +0800

    Delete generate-with-jar-swagger directory

--- generate-with-jar-swagger/generate.bat
+++
@@ -1,3 +0,0 @@
-powershell wget https://repo1.maven.org/maven2/io/swagger/codegen/v3/swagger-codegen-cli/3.0.27/swagger-codegen-cli-3.0.27.jar
-java -jar swagger-codegen-cli-3.0.27.jar generate -i ../viki3d-cars-api-v1.yaml -o generated-sources -c config.json
-pause
